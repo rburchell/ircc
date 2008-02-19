6d2bc8bc87fb33c7af66bf29db9d3de6af9a4fff
w00t <w00t@d8409493-2f4b-489e-a991-b2b196d7bf73>
2008-02-19 10:53:25 +0000

    Stylistic changes.
    git-svn-id: file://localhost/var/svnroot/ircc/trunk@11 d8409493-2f4b-489e-a991-b2b196d7bf73

--- ncurse.class.php
+++ ncurse.class.php
@@ -1,33 +1,19 @@
 
 <?php
+/*
+ * ircc - a handy, portable console irc client
+ *
+ * Copyright (C) 2008 Robin Burchell <w00t@inspircd.org>
+ * Copyright (C) 2003 rainman <rainman@darkwired.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
 
-/*---------------------------------------------------------
-		Vote torc! Vote insanity!
-
-		torc - torx irc client
-		Copyright (C) 2003 rainman <rainman@darkwired.org>
-
-		This program is free software; you can redistribute it and/or modify
-		it under the terms of the GNU General Public License as published by
-		the Free Software Foundation; either version 2 of the License, or
-		(at your option) any later version.
-
-		This program is distributed in the hope that it will be useful,
-		but WITHOUT ANY WARRANTY; without even the implied warranty of
-		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-		GNU General Public License for more details.
-
-		You should have received a copy of the GNU General Public License
-		along with this program; if not, write to the Free Software
-		Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
-		Class functions:
-			- provide simple access to an ncurses gui
-
----------------------------------------------------------*/
-
-class ncurse {
-
+class ncurse
+{
 	var $ncursesess;
 	var $mainwin;
 	var $userinputw;
@@ -43,12 +29,12 @@ class ncurse {
 	var $sendhislu;
 	var $scrollfd = 0;
 
-	function ncurse(){
-
+	function ncurse()
+	{
 		$this->stdin = fopen('php://stdin', 'r');
 
 		$this->ncursesess = ncurses_init();
-                ncurses_start_color();
+		ncurses_start_color();
 		ncurses_curs_set(0);
 
 		$this->mainwin = ncurses_newwin(0, 0, 0, 0);
@@ -73,7 +59,8 @@ class ncurse {
 	function addtoircout($line, $fd = -1)
 	{
 		// NEVER CALL THIS FUNCTION WITH INPUT WITH \n's
-		while(strlen($line) > $this->columns-3){
+		while(strlen($line) > $this->columns-3)
+		{
 			$this->ircout_content .= substr($line,0, $this->columns-6)."\n";
 			$line = '   '.substr($line, $this->columns-6);
 		}
@@ -89,12 +76,12 @@ class ncurse {
 
 	function buildircout($fd = 0)
 	{
-
 		$fd += 2;
 
 		$ex = explode("\n", $this->ircout_content);
 
-		for($x = 0; $x < $this->lines-2; $x++){
+		for($x = 0; $x < $this->lines-2; $x++)
+		{
 			ncurses_mvwaddstr($this->ircoutput, $x, 0, $this->cl);
 		}
 
@@ -104,10 +91,14 @@ class ncurse {
 
 		for($x = count($ex)-$fd; $x >= 0; $x--)
 		{
-			if($linesleft == 0){
+			if($linesleft == 0)
+			{
 				$x = -1;
-			} else {
-				if(!empty($ex[$x])){
+			}
+			else
+			{
+				if(!empty($ex[$x]))
+				{
 					$linesleft--;
 					ncurses_mvwaddstr($this->ircoutput, $linesleft, 0, $ex[$x]);
 				}
@@ -131,21 +122,30 @@ class ncurse {
 				$this->setuserinput();
 				$this->sendhislu = true;
 				return $usrp;
-			} elseif($c == chr(0x7F) || $c == chr(0x08)){
+			}
+			elseif($c == chr(0x7F) || $c == chr(0x08))
+			{
 				$this->userinputt = substr($this->userinputt, 0, strlen($this->userinputt)-1);
 				$this->setuserinput();
 				return false;
-			} elseif($c == chr(0x1B)){
-				if(stream_select($read = array($this->stdin), $write = NULL, $except = NULL, 0)){
+			}
+			elseif($c == chr(0x1B))
+			{
+				if(stream_select($read = array($this->stdin), $write = NULL, $except = NULL, 0))
+				{
 					$c = fgetc($this->stdin);
-					if($c == chr(0x5B)){
-						if(stream_select($read = array($this->stdin), $write = NULL, $except = NULL, 0)){
+					if($c == chr(0x5B))
+					{
+						if(stream_select($read = array($this->stdin), $write = NULL, $except = NULL, 0))
+						{
 							$c = fgetc($this->stdin);
 							$this->addtoircout("TORX: ".ord($c)."\n");
 							switch($c){
 							case chr(0x41):
-								if($this->sendhispt >= 0){
-									if(!$this->sendhislu){
+								if($this->sendhispt >= 0)
+								{
+									if(!$this->sendhislu)
+									{
 										$this->sendhispt--;
 										$this->sendhislu = true;
 									}
@@ -153,11 +153,13 @@ class ncurse {
 									$this->sendhispt--;
 									$this->setuserinput();
 								}
-							break;
+								break;
 
 							case chr(0x42):
-								if($this->sendhispt+1 < count($this->sendhis)){
-									if($this->sendhislu){
+								if($this->sendhispt+1 < count($this->sendhis))
+								{
+									if($this->sendhislu)
+									{
 										$this->sendhispt++;
 										$this->sendhislu = false;
 									}
@@ -165,11 +167,11 @@ class ncurse {
 									$this->userinputt = $this->sendhis[$this->sendhispt];
 									$this->setuserinput();
 								}
-							break;
+								break;
 
 							case chr(0x35):
 								fgetc($this->stdin);
-							break;
+								break;
 
 							case chr(0x36);
 								$this->scrollfd = $this->scrollfd-$this->lines;
@@ -181,8 +183,9 @@ class ncurse {
 						}
 					}
 				}
-					
-			} else {
+			}
+			else
+			{
 				$this->userinputt .= $c;
 				$this->setuserinput();
 				return false;
@@ -190,7 +193,8 @@ class ncurse {
 		}
 	}
 
-	function setuserinput($user, $chan){
+	function setuserinput($user, $chan)
+	{
 		ncurses_mvwaddstr($this->userinputw, 0, 0, $this->cl);
 		ncurses_mvwaddstr($this->userinputw, 0, 0, date("[H:i:s] ") . "[" . $user. "]");
 
@@ -199,12 +203,11 @@ class ncurse {
 		ncurses_wrefresh($this->userinputw);
 	}
 
-	function quit(){
+	function quit()
+	{
 		ncurses_clear();
 		ncurses_end();
 	}
-
-
 }
 
 ?>
