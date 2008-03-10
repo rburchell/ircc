9c88fdc3184a9557c5b099d272e0cd0d151abc98
w00t <w00t@d8409493-2f4b-489e-a991-b2b196d7bf73>
2008-03-10 20:02:43 +0000

    Add support for /window close <id>
    git-svn-id: file://localhost/var/svnroot/ircc/trunk@144 d8409493-2f4b-489e-a991-b2b196d7bf73

--- src/commands/window.command.inc.php
+++ src/commands/window.command.inc.php
@@ -2,5 +2,20 @@
 if ($this->output->IsBuffer($ex[1]))
 	$this->output->DrawBuffer($ex[1]);
 else
-	$this->output->Output(BUFFER_CURRENT, "That is not a valid window.");
+{
+	if ($ex[1] == "close")
+	{
+		if ($this->output->IsBuffer($ex[2]) && $ex[2] != 0 /* no closing root window. */)
+		{
+			$this->output->DeleteBuffer($ex[2]);
+			$this->output->Output(BUFFER_CURRENT, "Closed successfully.");
+		}
+		else
+		{
+			$this->output->Output(BUFFER_CURRENT, "This is an immortal window; only god may terminate it. (or, you fail, and it doesn't exist)");
+		}
+	}
+	else
+		$this->output->Output(BUFFER_CURRENT, "That is not a valid window or command.");
+}
 ?>
